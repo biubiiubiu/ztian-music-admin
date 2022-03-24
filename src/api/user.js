c68bff9bf01bf6388cc893d58277b8c98a81be45
zt <3141616128@qq.com>
2022-03-24 17:40:27 +0800

    feat:login,404,403

---
+++ src/api/user.js
@@ -0,0 +1,13 @@
+import { get, post } from './request';
+
+export const search = paging => {
+    return get('/users/', { params: paging });
+};
+
+export const me = () => {
+    return get('/users/me');
+};
+
+export const create = user => {
+    return post('/users', user);
+};
---
+++ src/composables/useUserSearch.js
@@ -0,0 +1,27 @@
+import { computed, onMounted, ref } from 'vue';
+import { search } from '../api/user';
+
+export const useUserSearch = pagination => {
+    const data = ref([]);
+
+    const fetchData = () => {
+        console.log(pagination.value);
+        const pageable = {
+            page: pagination.value.page - 1,
+            size: pagination.value.rowsPerPage
+        };
+        search(pageable).then(res => {
+            data.value = res.content;
+            pagination.value.page = res.number + 1;
+            pagination.value.rowsNumber = res.totalElements;
+        });
+    };
+
+    onMounted(fetchData);
+
+    return {
+        data,
+        pagination,
+        fetchData
+    };
+};
