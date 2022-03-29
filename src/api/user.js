95ef4adf6913fe46f3e5c44189fca0c0acf45af6
zt <3141616128@qq.com>
2022-03-29 09:43:56 +0800

    add:接口\注释

--- src/api/user.js
+++ src/api/user.js
@@ -1,13 +1,16 @@
 import { get, post } from './request';
 
+// 所有用户信息
 export const search = paging => {
     return get('/users/', { params: paging });
 };
 
+// 当前用户信息
 export const me = () => {
     return get('/users/me');
 };
 
+// 创建新用户
 export const create = user => {
     return post('/users', user);
 };
